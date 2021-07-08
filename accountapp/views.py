0b6373aa3aae8533f20e417826b4785c0a1a3aca
gnejf <gnejfejf789@naver.com>
2021-07-08 15:35:30 +0900

    7/1 class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 def hello_world(request):
     if request.method == 'Post' :
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'text': 'Post METHOD!'})
     else :
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD!'})
