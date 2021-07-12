22503ca902e430ade68cd1f35b14472c69bf1f81
gnejf <gnejfejf789@naver.com>
2021-07-12 14:48:51 +0900

    7/12 class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,8 +13,13 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_output': new_hello_world})
+                      context={'hello_world_list': hello_world_list})
     else:
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'hello_world_list': hello_world_list})
