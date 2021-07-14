21734854eee80e9d8f40516cac3512790001b8ac
gnejf <gnejfejf789@naver.com>
2021-07-14 10:22:45 +0900

    7/14 class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,3 +35,6 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+
--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -144,4 +144,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accountappp:login')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
